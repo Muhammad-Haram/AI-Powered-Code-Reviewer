7194a4e35b5f01fde5bb57c3301976910683725d
Muhammad-Haram <haramh643@gmail.com>
2025-02-13 16:53:30 +0500

    integratation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "prismjs/themes/prism-tomorrow.css";
+import prism from "prismjs";
+import Editor from "react-simple-code-editor";
+import axios from "axios";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [review, setReview] = useState("Code Review");
+  const [code, setCode] = useState(`function sum() {
+  return 1 + 1;
+}`);
+
+  useEffect(() => {
+    prism.highlightAll();
+  }, []);
+
+  async function reviewCode() {
+    try {
+      const response = await axios.post("http://localhost:3000/ai/get-review", {
+        code,
+      });
+      console.log(response.data);
+      setReview(response.data);
+    } catch (error) {
+      console.error("Error:", error);
+      alert("Failed to fetch review. Please try again.");
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main>
+      <div className="left">
+        <div className="code">
+          <Editor
+            value={code}
+            onValueChange={(code) => setCode(code)}
+            highlight={(code) =>
+              prism.highlight(code, prism.languages.javascript, "javascript")
+            }
+            padding={10}
+            style={{
+              fontFamily: '"Fira code", "Fira Mono", monospace',
+              fontSize: 16,
+              border: "1px solid #ddd",
+              borderRadius: "5px",
+              height: "100%",
+              width: "100%",
+            }}
+          />{" "}
+        </div>
+        <div className="review" onClick={reviewCode}>
+          Review
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <div className="right">{review}</div>
+    </main>
+  );
+};
 
-export default App
+export default App;
